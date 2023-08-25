56f8c86ff91fdc99d92d9f9768c7eacc4337ed56
Sai Jayanth Vattikonda <102982168+saijayanth-vattikonda@users.noreply.github.com>
2023-08-25 11:22:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var randomDiceImage1 = "dice" +randomNumber1+ ".png";
+var randomDiceImage2 = "dice"+randomNumber2+".png";
+var image1 = document.querySelectorAll("img")[0];
+var image2 = document.querySelectorAll("img")[1];
+image1.setAttribute("src", randomDiceImage1);
+image2.setAttribute("src", randomDiceImage2);
+
+if(randomNumber1>randomNumber2)
+{
+    document.querySelector("h1").innerHTML = "Player1 Wins !!";
+    document.querySelector("h1").style.color = "blue";
+}
+else if(randomNumber1<randomNumber2)
+{
+    document.querySelector("h1").innerHTML = "Player2 Wins !!";
+}
+else if(randomNumber1===randomNumber2)
+{
+    document.querySelector("h1").innerHTML = "Draw !!   ";
+    document.querySelector("h1").style.color = "yellow";
+}
